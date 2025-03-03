3fdd6fba1cbeddd8448f549986c9a87e009ed0cf
Chris Zaleski <zaleski.chris@gmail.com>
2025-03-03 09:37:39 -0500

    Update header notes

--- src/components/HeaderNotes.vue
+++ src/components/HeaderNotes.vue
@@ -16,11 +16,11 @@ const showNotes = ref(false)
         batting/pitching stats.</span><br/><br/>
         <div class="notes-section">Component Usage:</div>
         <ul>
-            <li>You can search for players by name. Both first name 
-                and last name are searched.</li>
-            <li>Sorting is available on select columns. (Hover the 
-                column header) </li>
-            <li>Use the checkboxes to select up to 5 players. Players' 
+            <li>All columns are sortable.</li>
+            <li>Text based filtering is enabled on appropriate columns. 
+                Unfortunately select/checkbox filtering is an enterprise 
+                (paid) option.</li>
+            <li>Use the checkboxes to select players. Players' 
                 OPS-by-year will be compared in a chart.</li>
             <li>NOTE: The OPS calculation does not include HBP as this 
                 was not included in the original data. Also the assumption 
@@ -32,27 +32,11 @@ const showNotes = ref(false)
             <li>Currently only OPS comparisons are implemented. Many 
                 other batting and pitching stats could be compared in 
                 the same way.</li>
-            <li>Filtering table columns.</li>
-            <li>Possibly replace the table altogether. Not sure I like 
-                this one.</li>
-            <li>Tons of other things :)</li>
-        </ul>
-        <div class="notes-section top-space">Issues:</div>
-        <ul>
-            <li>If a player has no batting stats, currently a simple 
-                alert box is fired. However the checkbox remains checked 
-                and the player remains in the chart. These need to both be 
-                removed. <br/><span class="italic">For now, unchecking that player manually 
-                will create the correct state.</span>
-            </li>
-            <li>
-                <span class="warning">!!!</span> Please avoid the "select all" 
-                checkbox in the table header. Since we have 1200+ rows, this 
-                causes the browser to hang for a few seconds. Data is not retrieved
-                for any number of players beyond 5, so the network traffic is avoided.
-                There does not appear to be a way to remove this checkbox outside of
-                editing the source code. (one of the reasons I'm not sold on this 
-                table implementation)
+                <li>If a player has no batting stats, currently a simple 
+                alert box is fired, however the checkbox remains checked. This
+                needs to be programatically unchecked. <br/>
+                For now, the user must uncheck that player
+                manually.
             </li>
         </ul>
     </div>
